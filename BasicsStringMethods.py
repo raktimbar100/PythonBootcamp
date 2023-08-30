43003c95797e7a0d3a15bec553fb4a6599ea46a2
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2023-08-30 20:26:17 +0530

    Create BasicsStringMethods.py

---
+++ BasicsStringMethods.py
@@ -0,0 +1,12 @@
+#String methods
+
+name="Youtube"
+print(len(name))
+print(name.lower())
+print(name.upper())
+print(name.capitalize())
+print(name.find('b'))
+print(name.count('u'))
+print(name.replace('Y','K'))
+print(name.isdigit())
+print(name.isalpha())
