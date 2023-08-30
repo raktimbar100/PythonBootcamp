b9c5886cdf245a58ac9e881719aacd4ac8b08927
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2023-08-30 20:11:25 +0530

    Update BasicDatatypes.py

--- BasicDatatypes.py
+++ BasicDatatypes.py
@@ -1,11 +1,15 @@
 print("I love it ")
 print("Its good")
 #variable:its a container for a value and behaves as the value it contains.
-name="Hi"
+name="Ram"
 print(type(name))
+print("Hello "+name)
 age=12
 print(type(age))
+print("Your age is "+str(age))
 height=78.92
 print(type(height))
+print("Your height is "+str(height))
 answer =False
 print(type(answer))
+print("Your answer is "+str(answer))
