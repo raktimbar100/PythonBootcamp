d5a6b89a7588419d87f2678cf8e521cdee8e804f
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2023-08-31 20:29:06 +0530

    Create NotesDay1.py

---
+++ NotesDay1.py
@@ -0,0 +1,91 @@
+#Basic Operators
+print(2+3)
+print(2*3)
+print(3-2)
+print(3/2)
+print(3%2)
+print(3//2)
+print(2**3)
+
+#variables
+
+name="Raktim"
+print(name)
+
+
+age=12
+print(age)
+
+answer = False
+print(answer)
+
+height=176.54
+print(height)
+
+number=1+2j
+print(number)
+
+list=[1,"Ram",78.9]
+list1=[2,"Soma",88.4]
+print(list)
+print(list1)
+
+tuple=("Ram Kumar",True,67)
+print(tuple)
+
+dict={1:"Ram Kumar",2:"Alex",3:"Raktim"}
+print(dict)
+
+set={"Rameswar",233}
+print(set)
+
+#How to declare variable
+
+#   demo=1 #right
+#   1demo=4 #wrong
+#   demo_1=5 #right
+#   @demo=9 #wrong
+#   de123mo=100 #right
+#   de123mo^d$=67 #wrong
+#   _=56 #right
+#   1_=90 #wrong
+#   ____="Ram IS A GOOD BOY" #right
+#   ?xyz=78 #wrong
+
+
+#delete variable 
+
+nv="hi"
+print(nv)
+#del nv
+#print(nv)
+
+
+#Datatypes
+
+age=12
+print(type(age))
+
+name="Ram"
+print(type(name))
+
+height=176.77
+print(type(height))
+
+answer=False
+print(type(answer))
+
+c=2+3j
+print(type(c))
+
+list=["Ram",1,45.66,True]
+print(type(list))
+
+tuple=(1,"Raktim",False)
+print(type(tuple))
+
+dict={1:"Ram",2:"Shyamm",3:1}
+print(type(dict))
+
+set={1,"Ram",67.88}
+print(type(set))
